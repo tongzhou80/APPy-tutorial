40da465521bc6b51ce8003c05ac93b14965350a8
Tong <tz@gatech.edu>
2025-06-11 03:53:18 -0400

    updated inc_appy.py

--- 05-increment/inc_appy.py
+++ 05-increment/inc_appy.py
@@ -1,12 +1,14 @@
 import numpy as np
 import time
+import appy
 
+@appy.jit(dump_final_appy=True)
 def increment(a):
-    for i in range(a.shape[0]):
-        a[i] += 1
+    for i in appy.prange(a.shape[0]):
+        a[i] = a[i] + 1
 
 def main():
-    a = np.empty(1_000_000, dtype=np.int32)
+    a = np.zeros(10_000_000, dtype=np.int32)
     start_time = time.time()
     increment(a)
     end_time = time.time()
@@ -15,4 +17,6 @@ def main():
     assert np.all(a == 1)
 
 if __name__ == "__main__":
+    main()
+    main()
     main()
